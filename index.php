1c2d36a909f947b21ded9ecaac442a039e495072
tof28 <boissierchristophe@gmail.com>
2022-09-23 12:06:15 +0200

    Premier commit - ajout de index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo '<table border>
+        <tr>
+            <th>Nombre</th>
+            <th>Carré</th>
+            <th>Racine</th>
+
+        </tr>';
+    
+for ($i = 1; $i < 11; $i++) 
+    {
+        echo '<tr>
+                <td>'.$i.'</td>
+                <td>'.$i * $i.'</td>
+                <td>'.sqrt($i).'</td>
+        </tr>';
+    }
+    echo '</table>';    
